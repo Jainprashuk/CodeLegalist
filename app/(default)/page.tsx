629323a27cf87c3db99091c9002e85fc226febea
Prashuk Jain <29jainprashuk@gmail.com>
2023-09-28 11:29:45 +0530

    files uploaded

---
+++ app/(default)/page.tsx
@@ -0,0 +1,21 @@
+export const metadata = {
+  title: 'Home - Simple',
+  description: 'Page description',
+}
+
+import Hero from '@/components/hero'
+import Features from '@/components/features'
+import FeaturesBlocks from '@/components/features-blocks'
+import Testimonials from '@/components/testimonials'
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Features />
+      <FeaturesBlocks />
+      {/* <Testimonials /> */}
+      {/* <Newsletter /> */}
+    </>
+  )
+}
